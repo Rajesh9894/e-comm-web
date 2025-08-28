39c3e61e208d9b0db9c6d2e9b5319ab3e230d76d
Rajesh <rajeshff220375@gmail.com>
2025-08-28 09:17:30 +0530

    updste settings

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -73,17 +73,14 @@ WSGI_APPLICATION = "ecommerce_site.wsgi.application"
 # ----------------------------
 # Database
 # ----------------------------
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///" + str(BASE_DIR / "db.sqlite3"))
+DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True if "postgres" in DATABASE_URL else False,
+    )
+}
 
 # ----------------------------
 # Password Validation
